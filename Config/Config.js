3e16702fa1b025322be566a23c61a5ee01eae49f
rahulbhuse16 <rahulbhuse2001@gmail.com>
2024-12-06 20:41:49 +0530

    node modules deleted

---
+++ Config/Config.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    migrationStoragePath: path.join(__dirname, '../migrations'),
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    migrationStoragePath: path.join(__dirname, '../migrations'),
+  },
+};
